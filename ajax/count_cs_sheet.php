086d5fa3ad2b609d05125026fea9413bb578d7f5
朝倉靖文 <y-asakura@lab-unlimited.com>
2025-05-09 14:08:56 +0900

    fix：別画面ログアウト対応

--- ajax/count_cs_sheet.php
+++ ajax/count_cs_sheet.php
@@ -3,5 +3,6 @@
 // 部品：件数表示（申請中の対応ログ）
 //======================================================================
 ?>
-<?php require_once '../_app/ssl_base.php'; ?>
+<?php /*暫定対応...ssl_baseだとログインページに遷移するため*/ ?>
+<?php require_once '../_app/base.php'; ?>
 <span class="badge badge-pill badge-danger p-empty-0"><?php appFuncStorage::load(appConfigPage::$path); ?></span>
