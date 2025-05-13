36282318d602122f0e94d5bc89dc6a2836a724d9
朝倉靖文 <y-asakura@lab-unlimited.com>
2025-05-13 16:05:22 +0900

    FIX：総客シートのプランプルダウン...プラン未定も火葬に含める

--- _module/js/form-sheet.php
+++ _module/js/form-sheet.php
@@ -24,7 +24,7 @@
         <?php appFuncMinify::minifySourceStart(); ?>
         const kasouPlan = {
             <?php foreach (appConfigFuneral::plan as $key => $plan): ?>
-                <?php if ($plan['category'] === appConfigFuneral::planCategoryKasou): ?>
+                <?php if ($plan['category'] != appConfigFuneral::planCategorySougi): ?>
                     <?php echo $key; ?>: "<?php echo $key; ?>",
                 <?php endif; ?>
             <?php endforeach; ?>
