f6f5c5699441c55afb697d43cc2a4038916522fc
朝倉靖文 <y-asakura@lab-unlimited.com>
2025-06-04 11:15:10 +0900

    feat：データ追加・変更：電話番号とユーザー

--- _app/config/user.php
+++ _app/config/user.php
@@ -16,6 +16,7 @@ class appConfigUser
     public const list = [
         '1' => ['userid' => '1', 'loginid' => 'tpf', 'password' => 'tp0135', 'username' => 'スタッフ', 'authority' => self::authorityStaff],
         '2' => ['userid' => '2', 'loginid' => 'master', 'password' => 'master', 'username' => '管理者', 'authority' => self::authorityManager],
-        '3' => ['userid' => '3', 'loginid' => 'test', 'password' => 'test0509', 'username' => 'TEST', 'authority' => self::authorityStaff],
+        '3' => ['userid' => '3', 'loginid' => 'test', 'password' => 'test0509', 'username' => 'スタッフⅡ', 'authority' => self::authorityStaff],
+        '4' => ['userid' => '4', 'loginid' => 'master2', 'password' => 'master2', 'username' => '管理者Ⅱ', 'authority' => self::authorityManager],
     ];
 }
